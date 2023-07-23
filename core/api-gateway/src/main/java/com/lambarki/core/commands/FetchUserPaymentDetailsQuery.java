c9d0b21f352b95cd7c11a38e853da814d205461b
YASSINE LAMBARKI <39433462+developpeurk@users.noreply.github.com>
2023-07-23 18:21:24 +0100

    Rétablir le suivi de version de Git pour les dossiers core et api-gateway

---
+++ core/api-gateway/src/main/java/com/lambarki/core/commands/FetchUserPaymentDetailsQuery.java
@@ -0,0 +1,11 @@
+package com.lambarki.core.commands;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data  @AllArgsConstructor
+public class FetchUserPaymentDetailsQuery {
+    private String userId;
+
+
+}
